4a5a9ad605f3a7c5838ff7823cad0f7659e60012
Om3259 <146020973+Om3259@users.noreply.github.com>
2025-04-01 17:17:45 -0400

    Add files via upload

---
+++ App.js
@@ -0,0 +1,15 @@
+
+import "./App.css";
+import Course from "./Components/Course";
+
+
+function App() {
+  return (
+    <div className="container">
+  
+      <Course></Course>
+    </div>
+  );
+}
+
+export default App;
